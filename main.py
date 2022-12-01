e1964385d31fe34a96dc5a2ccb23b00e6236e151
sik1j <karkisikij2@gmail.com>
2022-12-01 12:29:53 -0800

    Added gameState, validatedInput(), Menu(), learn(), settings(), exit()

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ def initEnv():
     print("Great! Have fun!\n")
     return env
 
-# Use the playSound() function below to play sounds. 
-# soundfilename does not include the .wav extension, 
+# Use the playSound() function below to play sounds.
+# soundfilename does not include the .wav extension,
 # e.g. playSound(apples,ENV) plays apples.wav
 def playSound(soundfilename,env):
     if env == "m":
@@ -29,8 +29,49 @@ def playSound(soundfilename,env):
 ENV = initEnv()
 ###############################################################
 
-
+### Global Variables ###
+gameState = {
+    'TOTAL_CSV_LINES' : len(open('blackfoot.csv').readlines()),
+    'learnCount' : 3
+}
 ### Functions ###
+# Validates input before returning it
+def validatedInput(string, min, max):
+    valid = False
+    while not valid:
+        try:
+            # might need to validated that input isn't a float as well
+            value = int(input(string))
+            print(value)
+            while value < min or value > max:
+                print(f'Please choose a number from {min} to {max}')
+                value = int(input(string))
+            valid = True
+        except:
+            print(f'Please choose a number from {min} to {max}')
+    return int(value)
+
+def Menu():
+    option = {'LEARN': 1, 'PLAY': 2, 'SETTINGS': 3, 'EXIT': 4}
+    exitFlag = False
+    while not exitFlag:
+        print(
+    """\nMAIN MENU
+    1. Learn - Word Flashcards
+    2. Play - Seek and Find Game
+    3. Settings - Change Difficulty
+    4. Exit"""
+            )
+        chosenOption = validatedInput('Choose an option: ', 1, 4)
+        if chosenOption == option.get('LEARN'):
+            learn()
+        elif chosenOption == option.get('PLAY'):
+            play()
+        elif chosenOption == option.get('SETTINGS'):
+            settings()
+        else:
+            exitFlag = exit()
+
 # Learn
     # ouput message explaining what the option does
     # requires 2 options back and continue
@@ -41,7 +82,18 @@ ENV = initEnv()
     # when user presses enter move on to next image
     # default amount of images is 3 (first 3 item in .csv file) can change num of images in settings
     # output menu at the end
-    
+def learn():
+    file = open('blackfoot.csv')
+    print('LEARN')
+    for index in range(1,gameState.get('learnCount')+1):
+        item = file.readline().split()[0]
+        image = draw.cmpt120image.getImage(f'images/{item}.png')
+        playSound(item, ENV)
+        canvas = draw.cmpt120image.getWhiteImage(400,300)
+        draw.distributeItems(canvas, image, 1)
+        draw.cmpt120image.showImage(canvas)
+        input(f'{index}. Press Enter to continue... ')
+
 # Play
     # ouput message explaining what the option does
     # requires 2 options back and continue
@@ -52,7 +104,7 @@ ENV = initEnv()
     # inform user if they are right or wrong (if wrong show the correct number of instences)
     # "Press enter to continue."
     # output menu at the end
-    
+
 # Settings
     # ouput message explaining what the option does
     # requires 2 options back and continue
@@ -61,50 +113,58 @@ ENV = initEnv()
     # changes amount of images to be shown in the learn section
     # changes which images can be shown in the play section
     # output menu at the end
-    
+def settings():
+    learnCount = gameState.get('learnCount')
+    maxLines = gameState.get('TOTAL_CSV_LINES')
+    print(f'You are currently learning {learnCount} words.')
+    newLearnCount = validatedInput(f'How many would you like to learn? (3-{maxLines})', 3, maxLines)
+
+    while newLearnCount < 3 or newLearnCount > maxLines:
+        print(f'{newLearnCount} is not allowed. Please choose again.')
+        newLearnCount = validatedInput(f'How many would you like to learn? (3-{maxLines})', 3, maxLines)
+    gameState['learnCount'] = newLearnCount
+
 # Exit?
     # ouput message explaining what the option does
     # asks for exit confirmation
         # need 2 options back and exit
     # exits program
+def exit():
+    print("Goodbye! ")
+    return True
 
-### Main ###
-# greets user
-print("Welcome! Before we start...")
-
-# verifies user gives valid input
-valid_input = False
-while Valid_input = False:
-    # asks user for whch interface they are using
-    user_interface = input("Are you using mu w/pygame0 (m), replit (r) or idle (i)?").lower()
-    # checks if user input is valid
-    if user_interface == "m" or user_interface == "r" or user_interface == "i":
-        valid_input = True
-        print("Great! Have fun!")
-    else:
-        print("Sorry, invalid input detected. Please try again.")
-
-### Make this a function ###
-# outputs options for user to interact with
-print("""MAIN MENU
-      1. Learn - Word Flashcards
-      2. Play - Seek and Find Game
-      3. Settings - Change Difficulty
-      4. Exit""")
-
-# verifies user gives valid input 
-valid_input = False
-while Valid_input = False:
-    # asks user what option they want to pick
-    menu_choice = int(input("Choose and option:"))
-    # verifies that the option entered is valid
-    if menu_choice <= 4 and menu_choice >= 0:
-        valid_input = True
-    else:
-        print("Sorry, invalid input detected. Please try again.")
-
-
-
-
-
-
+# ### Main ###
+Menu()
+# # greets user
+# print("Welcome! Before we start...")
+#
+# # verifies user gives valid input
+# valid_input = False
+# while Valid_input = False:
+#     # asks user for whch interface they are using
+#     user_interface = input("Are you using mu w/pygame0 (m), replit (r) or idle (i)?").lower()
+#     # checks if user input is valid
+#     if user_interface == "m" or user_interface == "r" or user_interface == "i":
+#         valid_input = True
+#         print("Great! Have fun!")
+#     else:
+#         print("Sorry, invalid input detected. Please try again.")
+#
+# ### Make this a function ###
+# # outputs options for user to interact with
+# print("""MAIN MENU
+#       1. Learn - Word Flashcards
+#       2. Play - Seek and Find Game
+#       3. Settings - Change Difficulty
+#       4. Exit""")
+#
+# # verifies user gives valid input
+# valid_input = False
+# while Valid_input = False:
+#     # asks user what option they want to pick
+#     menu_choice = int(input("Choose and option:"))
+#     # verifies that the option entered is valid
+#     if menu_choice <= 4 and menu_choice >= 0:
+#         valid_input = True
+#     else:
+#         print("Sorry, invalid input detected. Please try again.")
